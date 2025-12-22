4b75400c66f1b6a2d6d0ddfd2c48de6eb44ce793
Aryan2k4 <ag2k402122004@gmail.com>
2025-12-22 18:32:34 +0530

    Time: 61 ms (84.44%), Space: 12.9 MB (72.16%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,5 +1,22 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
-class Solution:  # Time = O(mn * log n) | Space = O(n)
-    def minDeletionSize(self, strs: List[str]) -> int:
-        return sum(col != sorted(col) for col in map(list, zip(*strs)))
+class Solution(object):
+    def minDeletionSize(self, strs):
+        """
+        Count columns that are not sorted lexicographically.
         
+        :type strs: List[str]
+        :rtype: int
+        """
+        count = 0
+        
+        # Iterate through each column index
+        for i in range(len(strs[0])):
+            last = 'a'  # Track the last seen character in this column
+            
+            # Check characters in this column from top to bottom
+            for letters in strs:
+                if letters[i] < last:  # Found an unsorted pair!
+                    count += 1
+                    break  # No need to check rest of this column
+                last = letters[i]  # Update last seen character
+        
+        return count
