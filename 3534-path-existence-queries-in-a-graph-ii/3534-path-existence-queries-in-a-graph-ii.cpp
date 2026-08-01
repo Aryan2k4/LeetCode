e4262f473c1f49a4e212786eb0cf36b0b4f598a1
Aryan Goswami <ag2k402122004@gmail.com>
2026-08-02 02:46:06 +0530

    Time: 361 ms (32.86%), Space: 395.3 MB (42.78%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,55 +1,43 @@
-constexpr int L=18, N=1e5+1;
-using int2=pair<int, int>;
-int up[L][N], pos[N]; 
-int2 xId[N];
-
 class Solution {
 public:
-    static int cnt(int u, int v, int L) {
-        if (u==v) return 0;
-        if (up[0][u]>=v) return 1;
-        // cannot reach v
-        if (up[L-1][u]<v) return -1; 
-
-        int step=0;
-        for (int j=L-1; j>=0; j--) { 
-            if (up[j][u]<v) { // Only jump when satisfied
-                step+=(1<<j);
-                u=up[j][u];
-            }
-        }
-        return step+1;
-    }
-
     vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        int maxL=bit_width((unsigned)n)+1;
-        for(int i=0; i<n; i++) 
-            xId[i]={nums[i], i};
-        
-        sort(xId, xId+n);
-        for (int i=0; i<n; i++)// pos of index in sorted xId
-            pos[xId[i].second]=i;
-        
-        //sliding window 
-        for (int l=0, r=0; l<n; l++) {
-            while (r+1<n && xId[r+1].first-xId[l].first<=maxDiff) 
+        vector<pair<int, int>> newNums(n);
+        for (int i = 0; i < n; i++)
+            newNums[i] = {nums[i], i};
+        sort(newNums.begin(), newNums.end());
+
+        vector<int> getI(n);
+        for (int i = 0; i < n; i++)
+            getI[newNums[i].second] = i;
+
+        vector<vector<int>> st(n, vector<int>(18));
+
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            if (r < i) r = i;
+            while (r + 1 < n &&
+                   newNums[r + 1].first - newNums[r].first <= maxDiff &&
+                   newNums[r + 1].first - newNums[i].first <= maxDiff)
                 r++;
-            up[0][l]=r;
+            st[i][0] = r;
         }
 
-        // Compute binary lifting tables
-        for (int j=1; j<maxL; j++) {
-            for (int i=0; i<n; i++) {
-                up[j][i]=up[j-1][up[j-1][i]];
-            }
-        }
+        for (int j = 1; j < 18; j++)
+            for (int i = 0; i < n; i++)
+                st[i][j] = st[st[i][j - 1]][j - 1];
 
-        const int qz=queries.size();
-        vector<int> ans(qz);
-        int i=0;
-        for (auto& q : queries) {
-            auto [u, v]=minmax(pos[q[0]], pos[q[1]]);
-            ans[i++]=cnt(u, v, maxL);
+        vector<int> ans(queries.size(), -1);
+        for (int i = 0; i < (int)queries.size(); i++) {
+            int a = getI[queries[i][0]];
+            int b = getI[queries[i][1]];
+            if (a > b) swap(a, b);
+            if (a == b) { ans[i] = 0; continue; }
+
+            int curr = a, steps = 0;
+            for (int j = 17; j >= 0; j--)
+                if (st[curr][j] < b) { curr = st[curr][j]; steps += (1 << j); }
+
+            ans[i] = (st[curr][0] >= b) ? steps + 1 : -1;
         }
         return ans;
     }
